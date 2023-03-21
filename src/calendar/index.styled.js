26bffe56c45c9e859580dd957e997061b98df1a6
Azizbek <khasanjonovich@gmail.com>
2023-03-21 14:21:37 +0900

    update

--- src/calendar/index.styled.js
+++ src/calendar/index.styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import { typography } from "styles";
 
 export const Page = styled.div`
   height: 100vh;
   width: 100vw;
+  ${typography.body_01_400}
 `;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import GlobalStyles from "@auth";
+import GlobalStyles from "styles";
 import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
