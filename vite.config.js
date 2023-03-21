5c5c0f51ce4931a8a428bf048b95e5670361c215
Azizbek <khasanjonovich@gmail.com>
2023-03-21 14:13:43 +0900

    for vite

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
   server: {
     port: 3000,
-    host: "0.0.0.0"
+    host: "0.0.0.0",
   },
-  plugins: [react({
-    include: "**/*.{jsx,tsx,js}",
-    babel:{
-      plugins: ["babel-plugin-styled-components"]
-    }
-  })],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname, "./src")
-    }
-  }
-})
+  plugins: [
+    react({
+      include: "**/*.{jsx,tsx,js}",
+      babel: {
+        plugins: ["babel-plugin-styled-components"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@/": path.resolve(__dirname, "./src/*"),
+      "@auth": path.resolve(__dirname, "./src/auth"),
+      "@calendar": path.resolve(__dirname, "./src/calendar"),
+      "@edit": path.resolve(__dirname, "./src/edit"),
+      "@record": path.resolve(__dirname, "./src/record"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    },
+  },
+});
