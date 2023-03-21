a16e03b230585f2c0fe2f62642a5149b1c5619f5
Azizbek <khasanjonovich@gmail.com>
2023-03-21 14:21:28 +0900

    update config

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,11 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@/": path.resolve(__dirname, "./src/*"),
-      "@auth": path.resolve(__dirname, "./src/auth"),
-      "@calendar": path.resolve(__dirname, "./src/calendar"),
-      "@edit": path.resolve(__dirname, "./src/edit"),
-      "@record": path.resolve(__dirname, "./src/record"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
+      auth: path.resolve(__dirname, "./src/auth"),
+      calendar: path.resolve(__dirname, "./src/calendar"),
+      dit: path.resolve(__dirname, "./src/edit"),
+      record: path.resolve(__dirname, "./src/record"),
+      styles: path.resolve(__dirname, "./src/styles"),
     },
   },
 });
