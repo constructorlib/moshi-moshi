785c8670f4726a33997a358da5b02fb1dbf01adc
Azizbek <khasanjonovich@gmail.com>
2023-04-13 17:08:19 +0900

    modal

---
+++ src/components/modal.styled.js
@@ -0,0 +1,8 @@
+import { default as _btn } from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import styled from "styled-components";
+
+export const Container = styled(Modal)`
+  padding: 1rem;
+  backdrop-filter: blur(5px);
+`;
