ff8f07f5cdb1e82f5e8c5719ab24542e86b6365a
Azizbek <khasanjonovich@gmail.com>
2023-04-17 07:54:55 +0900

    install svelte

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
-  },
 })
